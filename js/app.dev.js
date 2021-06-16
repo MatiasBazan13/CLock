40d5a3bbdf077ae9a8cc7ca93b0d2729a89c5939
Matias Bazan <matibazan13@gmail.com>
2021-06-16 15:50:28 -0300

    Change tags HTML and Style
    html,css and structure js changed

--- js/app.dev.js
+++ js/app.dev.js
@@ -1,16 +1,22 @@
-const reloj = document.querySelector(".reloj");
-const getHora = () => {
-
-    const fecha = new Date;
-    const tiempo = {
-        hora: fecha.getHours(),
-        minuto: fecha.getMinutes(),
-        segundos: fecha.getSeconds(),
-    };
-    
-    reloj.innerHTML = `${tiempo.hora} : ${tiempo.minuto} : ${tiempo.segundos}`;
+function reloj(){
+    let horas = document.getElementById('hora');
+    let minutos = document.getElementById('minutos');
+    let segundos = document.getElementById('segundos');
+    let ampm = document.getElementById('ampm');
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    var am = 'AM'
+    if(h > 12){
+        h = h-12;
+        var am = 'PM';
+    }
+    h = (h < 10) ? '0' + h : h;
+    m = (m < 10) ? '0' + m : m;
+    s = (s < 10) ? '0' + s : s;
+    horas.innerHTML = h;
+    minutos.innerHTML = m;
+    segundos.innerHTML = s;
+    ampm.innerHTML = am;
 }
-
-getHora();
-
-setInterval( getHora, 1000); 
+var intervalo = setInterval(reloj,1000)
